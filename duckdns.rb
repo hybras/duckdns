604fb581cef4e9ee7ce5287f277b7572df2ca47e
Hybras <24651269+hybras@users.noreply.github.com>
2022-07-29 08:20:13 -0400

    Update formula
    * Change url and homepage
    * Use homebrew service dsl
    * Add tailscale dependency
    * TODO: Update sha

--- duckdns.rb
+++ duckdns.rb
@@ -1,36 +1,27 @@
 require "formula"
 
 class Duckdns < Formula
-  homepage "http://jzelinskie.com/homebrew-duckdns"
-  url "https://github.com/jzelinskie/homebrew-duckdns/archive/1.0.tar.gz"
+  homepage "https://github.com/hybras/duckdns"
+  url "https://github.com/hybras/duckdns/archive/1.0.tar.gz"
   sha256 "87108c4bafb4d5b9c66dfb3cd3efa9f0cb152981ef40d763a11230247d48484c"
+  
+  depends_on "tailscale"
 
   def install
     bin.install 'duckdns'
   end
 
   test do
-    system "#{bin}/duckdns"
+    system bin/"duckdns"
   end
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/duckdns</string>
-      </array>
-      <key>StartInterval</key>
-      <integer>300</integer>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
-    EOS
+  
+  service do
+    run bin/"duckdns"
+    run_type :interval
+    interval 300
+    log_path var / "log" / "duckdns.log"
+    error_log_path var / "log" / "duckdns.log"
   end
+  
 end
 
